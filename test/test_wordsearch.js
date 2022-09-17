29b28a2988336e7eecab148679d97ee0fabe1db1
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-09-17 12:46:57 -0400

    Implement vertical functionality

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,53 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is contained in row", function() {
+    const result = wordSearch([
+      ['A', 'W', 'H', 'E', 'L', 'L', 'O', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'HELLO')
+
+    assert.isTrue(result);
+  });
+
+  
+  it("should return true if the word and input 2d are in different casing", function() {
+    const result = wordSearch([
+      ['A', 'W', 'h', 'E', 'l', 'L', 'O', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'Hello')
+    
+    assert.isTrue(result);
+  });
+  
+  it("should return true if the word is presesnted vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'h', 'B', 'l', 'L', 'O', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['E', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['L', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['P', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'Help')
+  
+    assert.isTrue(result);
+  });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// Function which flips rowns and columns
+const transposeArray = function(letters) {
+	result = []
+	for (let i = 0; i < letters[0].length; i++) {
+		result.push([]);
+		for(let j = 0; j < letters.length; j++) {
+			result[i].push(letters[j][i]);
+		}
+	}
+	return result;
+};
 
-module.exports = wordSearch
+const horizontalJoin =  (letters, word) => {
+	const arrayOfStrings = letters.map((ls) => ls.join('').toUpperCase());
+	for (l of arrayOfStrings) {
+		if (l.includes(word.toUpperCase())) return true;
+	}
+};
+
+const wordSearch = (letters, word) => { 
+	if (horizontalJoin(letters, word)) {
+		return true;
+	} else {
+		const verticalLetters = transposeArray(letters);
+		if(horizontalJoin(verticalLetters, word)) {
+			return true;
+		}
+		return false;
+	}
+};
+
+
+module.exports = wordSearch;
+
+
